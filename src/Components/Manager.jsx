bb5457100dec3eea129ce1d64b18e72176e54293
Shahil <shahilshaikh5718@gmail.com>
2026-02-07 22:26:37 +0530

    improve the ui

--- src/Components/Manager.jsx
+++ src/Components/Manager.jsx
@@ -13,23 +13,35 @@ function Manager() {
                     {/*content_size is an applied directive see the index.css for it's definition  */}
                     {/* we are using flex-grow: 1 (i.e. "grow" in tailwind) in this item , flex-grow allows the item to grow as much as possible to cover entire available space */}
 
-                    <div className='input_container flex flex-col items-center px-20 gap-y-2 mb-5'>
+                    <div className='input_container flex flex-col items-center px-20 gap-y-2 mb-5 relative '>
                         <input type="text" placeholder='Website URL' className='input_style' />
                         <input type="password" placeholder='Password' className='input_style' />
-                        <button className='bg-green2 p-2 h-auto w-1/4 rounded-4xl text-2xl text-green1 font-normal'>Add Password</button>
+                        <span className='  font-semibold cursor-pointer text-green1 absolute top-20 right-24 text-sm hover:text-green-300 transition-colors duration-300'>
+                            Show
+                        </span>
+                        <button className='submit_button bg-green4 py-2 px-4 h-auto w-fit rounded-4xl text-2xl text-white font-normal flex items-center justify-center gap-x-2 hover:bg-green-300 hover:text-green-700 transition-colors duration-300'>
+                            <lord-icon
+                                src="https://cdn.lordicon.com/fgxwhgfp.json"
+                                // this src defines  which icon to render.
+                                trigger="hover"
+                                target=".submit_button"
+                                // target defines the element on which the trigger will work. Here we want the animation to trigger when we hover on the button, so we set the target as the button's class name that is submit_button.
+                                colors="primary:#005461,secondary:#0c7779"
+                                className="w-8 h-8">
+                            </lord-icon>
+                            <span>Add Password</span>
+                        </button>
                         {/* flex-direction: column stretches items to full width, while row usually limits them to content width. And so by default the input boxes and the button takes full width when we set the parent for flex direction as column*/}
                     </div>
 
-                    <div className='h-1/4 bg-green2 grow rounded-4xl'>
-                    {/* One thing to note here, if we open the dev options in browser in the below then this box resizes on the y axis automatically
+                    <div className='h-1/4 border-4 border-green4 grow rounded-4xl '>
+                        {/* One thing to note here, if we open the dev options in browser in the below then this box resizes on the y axis automatically
                     by shrinking on the y axis. THis happens due to the having of flex-shrink: 1 peroperty which is the default property for flex items.  
                     setting it to 0 will stop resizing it.
                     But here setting it to 0 will still cause it to resize as the height is not fixed rather it is 1/4 th of the parent container*/}
                     </div>
                 </div>
             </div>
-
-
         </div>
     )
 }
